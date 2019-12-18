3a9c2f15c70ce5d2e6ff4a11e10c43a860b0eb7f
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-18 21:51:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,3 +11,8 @@ end
 def list(songs)
   songs.each_with_index { |song, idx| puts "#{idx + 1}. #{song}"}
 end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  users_response = gets.strip
+end
