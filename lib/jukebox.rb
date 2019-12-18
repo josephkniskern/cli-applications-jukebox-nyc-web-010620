6471ee1a2988184c77805c82d5224935235adb3a
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-18 21:57:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,4 +15,6 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   users_response = gets.strip
+  if (1..9).to_a.index(users_response.to_i) != nil
+    puts "Playing #{songs[users_response.to_i - 1]}"
 end
